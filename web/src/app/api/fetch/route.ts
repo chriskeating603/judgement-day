b91e6b2e29df05319d3fb7fac5bf59af717ad076
Justin <justintorre75@gmail.com>
2024-03-02 14:55:18 -0800

    no cache

--- web/src/app/api/fetch/route.ts
+++ web/src/app/api/fetch/route.ts
@@ -6,5 +6,13 @@ export async function GET(req: NextRequest) {
   const client = await db.connect();
   const res = await client.sql`SELECT * from users ORDER BY created_on DESC`;
   console.log(res);
-  return new NextResponse(JSON.stringify(res.rows));
+
+  const response = new NextResponse(JSON.stringify(res.rows));
+
+  // Set headers to disable caching
+  response.headers.set("Cache-Control", "no-store, max-age=0");
+  response.headers.set("Pragma", "no-cache"); // For HTTP/1.0 compatibility
+  response.headers.set("Expires", "0");
+
+  return response;
 }
